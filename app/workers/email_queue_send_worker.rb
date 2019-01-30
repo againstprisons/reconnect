e4fd847c9e35878b370f9453e3f25ab82699ad33
Alice Jenkinson <c@zero-x.nz>
2019-01-30 13:22:12 +1300

    workers: Add logging to EmailQueueSendWorker

--- app/workers/email_queue_send_worker.rb
+++ app/workers/email_queue_send_worker.rb
@@ -4,24 +4,37 @@ class ReConnect::Workers::EmailQueueSendWorker
   def perform
     ReConnect.initialize if ReConnect.app.nil?
 
-    ReConnect::Models::EmailQueue.where(:queue_status => 'queued').all.each do |qm|
+    messages = ReConnect::Models::EmailQueue.where(:queue_status => 'queued')
+    logger.info("Queued message count: #{messages.count}")
+
+    messages.all.each do |qm|
+      logger.info("Message #{qm.id}: starting")
+
       # save state as 'sending'
       qm.queue_status = 'sending'
       qm.save
 
       begin
-        qm.generate_messages_chunked.each do |message|
-          message.deliver!
+        chunks = qm.generate_messages_chunked
+        logger.info("Message #{qm.id}: chunk count #{chunks.count}")
+
+        chunks.each_index do |i|
+          logger.info("Message #{qm.id}: sending chunk #{i}")
+          chunks[i].deliver!
         end
 
         qm.queue_status = 'sent'
         qm.save
-      rescue => e
-        puts "Error while sending message #{qm.id}: #{e.class.name}: #{e}"
-        puts e.traceback if e.respond_to?(:traceback)
 
+        logger.info("Message #{qm.id}: sent!")
+
+      rescue => e
         qm.queue_status = 'error'
         qm.save
+
+        errmsg = "Message #{qm.id}: errored - #{e.class.name}: #{e}\n"
+        errmsg += "Traceback: #{e.traceback}" if e.respond_to?(:traceback)
+        logger.error(errmsg)
       end
     end
   end
