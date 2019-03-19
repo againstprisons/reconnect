89a2614f1e6f8802524f26a9a0882f026839c88a
Alice Jenkinson <c@zero-x.nz>
2019-03-19 16:47:37 +1300

    system/penpal: Add searching for penpals (closes #7)

--- app/controllers/system_penpal_controller.rb
+++ app/controllers/system_penpal_controller.rb
@@ -1,7 +1,7 @@
 class ReConnect::Controllers::SystemPenpalController < ReConnect::Controllers::ApplicationController
   add_route :get, "/"
   add_route :post, "/by-id", :method => :by_id
-  add_route :post, "/by-name", :method => :by_name
+  add_route :post, "/search", :method => :search
   add_route :get, "/clear-copied", :method => :clear_copied
 
   def index
@@ -23,7 +23,7 @@ class ReConnect::Controllers::SystemPenpalController < ReConnect::Controllers::A
 
     id = request.params["id"]&.strip.to_i
     if id.zero?
-      flash :error, t(:'system/penpal/search_failed')
+      flash :error, t(:'system/penpal/search/failed')
       return redirect to('/system/penpal')
     end
 
@@ -36,21 +36,51 @@ class ReConnect::Controllers::SystemPenpalController < ReConnect::Controllers::A
     redirect to("/system/penpal/#{penpal.id}")
   end
 
-  def by_name
+  def search
     return halt 404 unless logged_in?
     return halt 404 unless has_role?("system:penpal:access")
 
-    flash :error, "unimplemented"
-    return redirect to('/system/penpal')
+    @title = t(:'system/penpal/search/results/title')
 
-    penpal_name = request.params["name"]&.strip
-    if penpal_name.nil? || penpal_name == ""
-      flash :error, t(:'system/penpal/search_failed')
+    search_type = request.params["type"]&.strip&.downcase
+    search_term = request.params["search"]&.strip&.downcase
+    if search_type.nil? || search_type.empty? || search_term.nil? || search_term.empty?
+      flash :error, t(:'system/penpal/search/failed')
       return redirect to('/system/penpal')
     end
 
-    # TODO: search by name once indexing works
-    #redirect to("/system/penpal/#{penpal.id}")
+    ids = ReConnect::Models::PenpalFilter.perform_filter(search_type, search_term).map(&:penpal_id).uniq
+    if ids.count.zero?
+      flash :error, t(:'system/penpal/search/failed')
+      return redirect to('/system/penpal')
+    end
+
+    penpals = ids.map{|id| ReConnect::Models::Penpal[id]}.compact.sort{|a, b| a.id <=> b.id}
+    if penpals.count.zero?
+      flash :error, t(:'system/penpal/search/failed')
+      return redirect to('/system/penpal')
+    end
+
+    # go directly to penpal page if only one penpal was found
+    if penpals.count == 1
+      return redirect to("/system/penpal/#{penpals.first.id}")
+    end
+
+    @penpals = penpals.map do |pp|
+      {
+        :id => pp.id,
+        :name => pp.get_name,
+        :link => url("/system/penpal/#{pp.id}"),
+      }
+    end
+
+    # if multiple are found, render a search results page
+    return haml(:'system/layout', :locals => {:title => @title}) do
+      haml(:'system/penpal/search_results', :layout => false, :locals => {
+        :title => @title,
+        :penpals => @penpals,
+      })
+    end
   end
 
   def clear_copied
