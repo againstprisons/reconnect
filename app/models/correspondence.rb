556282a58536831d8235b38ddcfd53bf4c92ee81
Alice Jenkinson <alice@jenkinson.nz>
2019-05-04 16:22:29 +1200

    models/correspondence: Add framework for sending emails into prisons

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -1,3 +1,5 @@
+require 'reverse_markdown'
+
 class ReConnect::Models::Correspondence < Sequel::Model(:correspondence)
   def self.find_for_relationship(relationship)
     [
@@ -16,6 +18,9 @@ class ReConnect::Models::Correspondence < Sequel::Model(:correspondence)
     actioning_user = ReConnect::Models::User[self.actioning_user]
     actioning_user_name = actioned ? actioning_user.decrypt(:name) : nil
 
+    has_been_sent = self.sent.nil? || self.sent != "no"
+    sending_method = has_been_sent ? self.sent.to_s : nil
+
     {
       :id => self.id,
       :creation => self.creation,
@@ -27,11 +32,29 @@ class ReConnect::Models::Correspondence < Sequel::Model(:correspondence)
       :receiving_is_incarcerated => penpal_receiving.is_incarcerated,
 
       :this_user_sent => penpal_sending.id == current_penpal,
+      :has_been_sent => has_been_sent,
+      :sending_method => sending_method,
 
-      :actioned => actioned,
+      :actioned => actioned || (sending_method == "email"),
       :actioning_user => actioning_user,
       :actioning_user_name => actioning_user_name,
     }
+  end
+
+  def send!
+    penpal_sending = ReConnect::Models::Penpal[self.sending_penpal]
+    penpal_receiving = ReConnect::Models::Penpal[self.receiving_penpal]
+    relationship = ReConnect::Models::PenpalRelationship.find_for_penpals(penpal_sending, penpal_receiving)
+    return unless relationship
+    return unless penpal_receiving.is_incarcerated
+
+    if relationship.email_approved
+      self.send_email_to_prison!
+      self.sent = "email"
+      self.save
+    else
+      self.send_alert!
+    end
   end
 
   def send_alert!
@@ -41,6 +64,10 @@ class ReConnect::Models::Correspondence < Sequel::Model(:correspondence)
     return unless relationship
 
     if penpal_receiving.is_incarcerated
+      # don't send an alert if automatic emails into prison have been approved
+      # for this penpal relationship
+      return if relationship.email_approved
+
       template = "correspondence_to_incarcerated"
 
       url = Addressable::URI.parse(ReConnect.app_config["base-url"])
@@ -86,6 +113,60 @@ class ReConnect::Models::Correspondence < Sequel::Model(:correspondence)
     email.encrypt(:subject, "New correspondence") # TODO: translation
     email.encrypt(:recipients, recipients)
     email.save
+  end
+
+  def send_email_to_prison!(force = false)
+    penpal_sending = ReConnect::Models::Penpal[self.sending_penpal]
+    penpal_receiving = ReConnect::Models::Penpal[self.receiving_penpal]
+    relationship = ReConnect::Models::PenpalRelationship.find_for_penpals(penpal_sending, penpal_receiving)
+    return unless relationship
+    return unless penpal_receiving.is_incarcerated
+
+    # check that this relationship has email enabled, if it doesn't (and force
+    # is not set) bail
+    return unless force || relationship.email_approved
+
+    # get prison info
+    prison = ReConnect::Models::Prison[penpal_receiving.decrypt(:prison_id).to_i]
+    return unless prison
+    return if prison.email_address.nil?
+    prison_email = prison.decrypt(:email_address)&.strip&.downcase
+    return if prison_email.nil? || prison_email.empty?
+    recipients = JSON.dump({
+      "mode" => "list",
+      "list" => [
+        prison_email,
+      ]
+    })
+
+    # get file info for this correspondence, bail if it's not text/html (as it
+    # should be), read in content
+    file = ReConnect::Models::File.where(:file_id => self.file_id).first
+    return unless file
+    html_part = file.decrypt_file
+
+    # construct plain-text version of the HTML contents using reverse-markdown
+    text_part = ReverseMarkdown.convert(html_part, :unknown_tags => :bypass)
+
+    # subject has to be "#{prisoner_name}, #{prisoner_number}"
+    subject = [
+      penpal_receiving.get_name,
+      ", ",
+      penpal_receiving.decrypt(:prisoner_number) || 'unknown prisoner number',
+    ].join("")
+
+    # create email queue entry
+    email = ReConnect::Models::EmailQueue.new(:queue_status => "preparing")
+    email.save
+    email.queue_status = "queued"
+    email.annotate_subject = false
+    email.encrypt(:subject, subject)
+    email.encrypt(:recipients, recipients)
+    email.encrypt(:content_text, text_part)
+    email.encrypt(:content_html, html_part)
+    email.save
+
+    email
   end
 
   def delete!
