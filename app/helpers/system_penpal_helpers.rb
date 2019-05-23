85c97e5b0453e00364abf8e4eca3551905cf0268
Alice Jenkinson <alice@jenkinson.nz>
2019-05-24 04:08:24 +1200

    helpers/system_penpal: Refactor SystemPenpalHelpers

--- app/helpers/system_penpal_helpers.rb
+++ app/helpers/system_penpal_helpers.rb
@@ -1,48 +1,50 @@
 module ReConnect::Helpers::SystemPenpalHelpers
   def penpal_view_data(pp)
-    name_a = pp.get_name
-    name = name_a.map{|x| x == "" ? nil : x}.compact.join(" ")
-    name = "(unknown)" if name.nil? || name&.strip&.empty?
-
     data = {
       :id => pp.id,
-      :name => name,
-      :name_a => name_a,
-      :is_incarcerated => pp.is_incarcerated,
+      :display_fields => [
+        [t(:'penpal_id'), pp.id],
+      ],
     }
 
-    data[:display_fields] = [
-      [t(:'name'), data[:name]],
-      [t(:'penpal_id'), data[:id]],
-      [t(:'is_incarcerated'), data[:is_incarcerated] ? 'yes' : 'no'],
+    # name
+    data[:name_a] = name_a = pp.get_name
+    name = name_a.map{|x| x == "" ? nil : x}.compact.join(" ")
+    name = "(unknown)" if name.nil? || name&.strip&.empty?
+    data[:name] = name
+    data[:display_fields] << [t(:'name'), name]
+
+    # user ID
+    data[:user_id] = pp.user ? pp.user.id : nil
+    data[:display_fields] << [t(:'user_id'), pp.user.id] if pp.user
+
+
+    # incarcerated status
+    data[:display_fields] << [
+      t(:'is_incarcerated'),
+      data[:is_incarcerated] ? 'yes' : 'no'
     ]
 
-    data[:user_id] = pp.user.id if pp.user
-
     if pp.is_incarcerated
+      # prisoner number
       prisoner_number = pp.decrypt(:prisoner_number)&.strip
       prisoner_number = "(unknown)" if prisoner_number.nil? || prisoner_number.empty?
       data[:prisoner_number] = prisoner_number 
       data[:display_fields] << [t(:'prisoner_number'), prisoner_number]
 
-=begin
-      address = pp.decrypt(:address)&.split("\n")&.map(&:strip)
-      unless address.nil? || address.empty?
-        data[:address] = address&.join("\n")
-        data[:display_fields] << [t(:'address'), address&.join(" / ")]
-      end
-=end
+      # prison info
+      begin
+        prison = ReConnect::Models::Prison[pp.decrypt(:prison_id).to_i]
+        if prison
+          data[:prison] = {
+            :id => prison.id,
+            :name => prison.decrypt(:name),
+          }
 
-      prison = ReConnect::Models::Prison[pp.decrypt(:prison_id).to_i]
-      if prison
-        data[:prison] = {
-          :id => prison.id,
-          :name => prison.decrypt(:name),
-        }
-
-        data[:display_fields] << [t(:'prison'), data[:prison][:name]]
-      else
-        data[:display_fields] << [t(:'prison'), '(unknown)']
+          data[:display_fields] << [t(:'prison'), data[:prison][:name]]
+        else
+          data[:display_fields] << [t(:'prison'), '(unknown)']
+        end
       end
     end
 
