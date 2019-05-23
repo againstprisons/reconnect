06649ac0d30611793914798343f392d0f27dd986
Alice Jenkinson <alice@jenkinson.nz>
2019-05-24 04:09:59 +1200

    helpers/system_penpal: Show last correspondence date in penpal view

--- app/helpers/system_penpal_helpers.rb
+++ app/helpers/system_penpal_helpers.rb
@@ -18,6 +18,37 @@ module ReConnect::Helpers::SystemPenpalHelpers
     data[:user_id] = pp.user ? pp.user.id : nil
     data[:display_fields] << [t(:'user_id'), pp.user.id] if pp.user
 
+    # get last correspondence
+    begin
+      data[:last_correspondence] = nil
+      last_sent = ReConnect::Models::Correspondence
+        .where(:sending_penpal => pp.id)
+        .order(Sequel.desc(:creation))
+        .first
+      last_received = ReConnect::Models::Correspondence
+        .where(:receiving_penpal => pp.id)
+        .order(Sequel.desc(:creation))
+        .first
+      last = [last_sent, last_received]
+        .compact
+        .sort{|a, b| b.creation <=> a.creation}
+        .first
+
+      if last
+        data[:last_correspondence] = {
+          :model => last,
+          :creation => last.creation,
+        }
+
+        data[:display_fields] << [
+          t(:'last_correspondence'),
+          [
+            last.creation.strftime("%Y-%m-%d %H:%M"),
+            "(#{pretty_time(last.creation)})"
+          ].join(" "),
+        ]
+      end
+    end
 
     # incarcerated status
     data[:display_fields] << [
