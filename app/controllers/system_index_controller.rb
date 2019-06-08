0e7ba302c9702207e61aeb2be296b7f1a45569b1
Alice Jenkinson <alice@jenkinson.nz>
2019-06-09 05:50:58 +1200

    system: Add list of unsent correspondence to admin dashboard

--- app/controllers/system_index_controller.rb
+++ app/controllers/system_index_controller.rb
@@ -6,10 +6,18 @@ class ReConnect::Controllers::SystemIndexController < ReConnect::Controllers::Ap
     return halt 404 unless has_role?("system:access")
 
     @title = t(:'system/title')
+    @to_action = []
+    if has_role?("system:penpal:access")
+      @to_action = ReConnect::Models::Correspondence
+        .where(:sent => 'no')
+        .map(&:get_data)
+        .reject{|x| x[:actioned]}
+    end
 
     haml(:'system/layout', :locals => {:title => @title}) do
       haml(:'system/index', :layout => false, :locals => {
         :title => @title,
+        :to_action => @to_action,
         :counts => {
           :penpals => ReConnect::Models::Penpal.count,
           :relationships => ReConnect::Models::PenpalRelationship.count,
