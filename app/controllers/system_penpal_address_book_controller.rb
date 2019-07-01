6c47a7afcbfeb70235e54a945106ef9091e159a0
Alice Jenkinson <alice@jenkinson.nz>
2019-07-02 03:39:30 +1200

    system/penpal: Add 'address book' view of all incarcerated penpals

---
+++ app/controllers/system_penpal_address_book_controller.rb
@@ -0,0 +1,47 @@
+class ReConnect::Controllers::SystemPenpalAddressBookController < ReConnect::Controllers::ApplicationController
+  include ReConnect::Helpers::SystemPenpalHelpers
+  add_route :get, "/"
+
+  def index
+    return halt 404 unless logged_in?
+    return halt 404 unless has_role?("system:penpal:access")
+
+    @title = t(:'system/penpal/address_book/title')
+
+    penpals_by_status = {}
+    ReConnect::Models::Penpal.where(:is_incarcerated => true).each do |pp|
+      pp_d = penpal_view_data(pp)
+      relationships = pp.relationships.map do |r|
+        other_party = r.penpal_one
+        other_party = r.penpal_two if other_party == pp.id
+        other_party = ReConnect::Models::Penpal[other_party]
+        next nil unless other_party
+
+        other_party_name = other_party.get_name
+        other_party_name = other_party_name.map{|x| x == "" ? nil : x}.compact.join(" ")
+
+        {
+          :id => r.id,
+          :link => url("/system/penpal/relationship/#{r.id}"),
+          :other_party_name => other_party_name,
+        }
+      end.compact
+
+      penpals_by_status[pp_d[:status]] ||= []
+      penpals_by_status[pp_d[:status]] << {
+        :obj => pp,
+        :pp_d => pp_d,
+        :relationships => relationships,
+      }
+    end
+
+    @pp_statuses = ReConnect.app_config['penpal-statuses'].map do |status|
+      [status, penpals_by_status[status] || []]
+    end
+
+    haml(:'system/penpal/address_book/index', :locals => {
+      :title => @title,
+      :pp_statuses => @pp_statuses,
+    })
+  end
+end
