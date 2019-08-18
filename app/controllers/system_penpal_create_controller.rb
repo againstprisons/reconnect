b5c68f958a8c7b321d3a0549142b6d4618b46fd8
Alice Jenkinson <alice@jenkinson.nz>
2019-08-18 21:17:11 +1200

    Add status/prisoner number/prison fields to penpal creation
    Clarify that the penpal creation is for inside penpals only, and set the
    is_incarcerated flag to true on all penpals created that way. Adds a
    link to the user creation page on the penpal creation page, and vice
    versa.

--- app/controllers/system_penpal_create_controller.rb
+++ app/controllers/system_penpal_create_controller.rb
@@ -9,11 +9,18 @@ class ReConnect::Controllers::SystemPenpalCreateController < ReConnect::Controll
     return halt 404 unless has_role?("system:penpal:create")
 
     @title = t(:'system/penpal/create/title')
+    @prisons = ReConnect::Models::Prison.all.map do |p|
+      {
+        :id => p.id,
+        :name => p.decrypt(:name),
+      }
+    end
 
     if request.get?
       return haml(:'system/layout', :locals => {:title => @title}) do
         haml(:'system/penpal/create', :layout => false, :locals => {
           :title => @title,
+          :prisons => @prisons,
         })
       end
     end
@@ -25,11 +32,27 @@ class ReConnect::Controllers::SystemPenpalCreateController < ReConnect::Controll
       return redirect request.path
     end
 
-    @penpal = ReConnect::Models::Penpal.new(:user_id => nil)
+    pp_prisoner_number = request.params["prisoner_number"]&.strip
+
+    pp_status = request.params["status"]&.strip
+    if !(ReConnect.app_config['penpal-statuses'].include?(pp_status))
+      pp_status = ReConnect.app_config['penpal-status-default']
+    end
+
+    pp_prison = request.params["prison"]&.strip&.downcase.to_i
+    if pp_prison.nil? || pp_prison.zero?
+      pp_prison = nil
+    else
+      pp_prison = ReConnect::Models::Prison[pp_prison]&.id
+    end
+
+    @penpal = ReConnect::Models::Penpal.new(:user_id => nil, :is_incarcerated => true)
     @penpal.save
     @penpal.encrypt(:first_name, pp_first_name)
     @penpal.encrypt(:last_name, pp_last_name)
-    @penpal.encrypt(:status, ReConnect.app_config['penpal-status-default'])
+    @penpal.encrypt(:prisoner_number, pp_prisoner_number)
+    @penpal.encrypt(:status, pp_status)
+    @penpal.encrypt(:prison_id, pp_prison)
     @penpal.save
 
     ReConnect::Models::PenpalFilter.create_filters_for(@penpal)
