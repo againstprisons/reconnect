ab6611a9b0107583b40c0b962f0c9141acd41c7f
Alice Jenkinson <alice@jenkinson.nz>
2019-08-18 20:48:11 +1200

    Add loading/error messages to rich editor creation

--- assets/js/rich_editor.js
+++ assets/js/rich_editor.js
@@ -1,3 +1,11 @@
+let loadingMessage = "Loading editor, please wait a moment..."
+let genericErrorMessage = (
+  "An error occurred trying to load the editor, " +
+  "your message may not display correctly. " +
+  "Refresh the page to try again."
+)
+
+let editorMaxTries = 10
 var editors = {}
 var editorTries = 1
 let editorConfig = {
@@ -18,15 +26,49 @@ let editorConfig = {
 }
 
 export const enableEditors = () => {
+  const setMessage = (classes, message) => {
+    var baseClasses = 'message js-message'
+    var el = document.getElementById('js-rich_editor-message')
+    if (!el) {
+      el = document.createElement('div')
+      el.id = 'js-rich_editor-message'
+
+      let messageContainer = document.querySelectorAll('label[for="content"]')
+      if (messageContainer.length > 0) {
+        messageContainer[0].insertAdjacentElement('beforebegin', el)
+      } else {
+        let bodyContainer = document.querySelectorAll('.body-container')[0]
+        bodyContainer.prepend(el)
+      }
+    }
+
+    if (classes) {
+      el.className = `${baseClasses} ${classes}`
+    } else {
+      el.className = baseClasses
+    }
+
+    if (message) {
+      el.style.display = 'block'
+      el.innerHTML = message
+    } else {
+      el.style.display = 'none'
+      el.innerHTML = ''
+    }
+  }
+
+
   let elements = document.querySelectorAll('.rich-editor')
   if (elements.length === 0) return;
 
   console.log(`Trying to load editors: try ${editorTries}`)
+  setMessage('message-warning', loadingMessage)
 
   if (typeof(ClassicEditor) === "undefined") {
     editorTries = editorTries + 1
-    if (editorTries > 5) {
+    if (editorTries > editorMaxTries) {
       console.error("Timed out trying to load editors")
+      setMessage('message-error', genericErrorMessage)
       return
     }
 
@@ -40,8 +82,10 @@ export const enableEditors = () => {
     var elid = (el.id == "" ? "unknown" : el.id)
     ClassicEditor.create(el, editorConfig).then((editor) => {
       editors[elid] = editor
+      setMessage('', undefined)
     }).catch((e) => {
       console.error(`Error while creating editor (id ${elid}): `, e)
+      setMessage('message-error', genericErrorMessage)
     })
   })
 }
