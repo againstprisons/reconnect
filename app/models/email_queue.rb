10d8fb4a4bd5fc72446900674021b8b433b469c5
Alice Jenkinson <alice@jenkinson.nz>
2019-08-26 11:19:13 +1200

    Restore email chunk size to 25

--- app/models/email_queue.rb
+++ app/models/email_queue.rb
@@ -2,7 +2,7 @@ require 'tilt/erb'
 require 'ostruct'
 
 class ReConnect::Models::EmailQueue < Sequel::Model(:email_queue)
-  EMAIL_CHUNK_SIZE = 10
+  EMAIL_CHUNK_SIZE = 25
 
   include ReConnect::Helpers::EmailTemplateHelpers
 
