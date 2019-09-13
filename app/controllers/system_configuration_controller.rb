f79ea158995f6b39cb52bb174749ad80f33cd45c
Alice Jenkinson <alice@jenkinson.nz>
2019-09-13 19:28:44 +1200

    Add form to /system/configuration to create administration profile

--- app/controllers/system_configuration_controller.rb
+++ app/controllers/system_configuration_controller.rb
@@ -2,12 +2,16 @@ class ReConnect::Controllers::SystemConfigurationController < ReConnect::Control
   add_route :get, "/"
   add_route :post, "/toggle", :method => :toggle
   add_route :post, "/refresh", :method => :refresh
+  add_route :post, "/create-admin-penpal", :method => :create_admin_penpal
 
   def index
     return halt 404 unless logged_in?
     return halt 404 unless has_role?("system:configuration:access")
 
     @title = t(:'system/configuration/title')
+
+    admin_pid = ReConnect.app_config['admin-profile-id']&.to_i
+    admin_profile_exists = !(admin_pid.nil? || admin_pid.zero?) && !(ReConnect::Models::Penpal[admin_pid].nil?)
 
     signups_enabled = ReConnect::Models::Config.where(:key => "signups").first&.value == "yes"
     signups_pending = ReConnect.app_config["signups"] != signups_enabled
@@ -45,6 +49,7 @@ class ReConnect::Controllers::SystemConfigurationController < ReConnect::Control
       haml(:'system/configuration/index', :layout => false, :locals => {
         :title => @title,
         :quick_toggles => quick_toggles,
+        :admin_profile_exists => admin_profile_exists,
       })
     end
   end
@@ -126,5 +131,66 @@ class ReConnect::Controllers::SystemConfigurationController < ReConnect::Control
     end
 
     redirect to("/system/configuration")
+  end
+
+  def create_admin_penpal
+    return halt 404 unless logged_in?
+    return halt 404 unless has_role?("system:configuration:admin_penpal")
+
+    # verify admin penpal doesn't already exist
+    admin_pid = ReConnect.app_config['admin-profile-id']&.to_i
+    if !admin_pid.nil? && !admin_pid.zero? && !(ReConnect::Models::Penpal[admin_pid].nil?)
+      return halt 418
+    end
+
+    # enable maintenance mode
+    maint_cfg = ReConnect::Models::Config.where(:key => 'maintenance').first
+    unless maint_cfg
+      maint_cfg = ReConnect::Models::Config.new(:key => 'maintenance', :type => 'bool', :value => 'no')
+    end
+    maint_enabled = maint_cfg.value == 'yes'
+    maint_cfg.value = 'yes'
+    maint_cfg.save
+
+    # create admin penpal
+    admin_penpal = ReConnect::Models::Penpal.new
+    admin_penpal.save # get ID
+    admin_penpal.encrypt(:first_name, ReConnect.app_config['org-name'])
+    admin_penpal.encrypt(:last_name, "Administration Profile")
+    admin_penpal.is_incarcerated = false
+    admin_penpal.is_advocacy = false
+    admin_penpal.correspondence_guide_sent = false
+    admin_penpal.save
+
+    # save profile id to config
+    cfg = ReConnect::Models::Config.where(:key => 'admin-profile-id').first
+    unless cfg
+      cfg = ReConnect::Models::Config.new(:key => 'admin-profile-id', :type => 'number')
+    end
+    cfg.value = admin_penpal.id.to_s
+    cfg.save
+
+    # refresh
+    ReConnect.app_config_refresh_pending << 'admin-profile-id'
+    ReConnect.app_config_refresh
+
+    # create job to add relationships
+    rjobid = ReConnect::Workers::CreateAdministratorProfileRelationshipWorker.perform_async
+
+    # success alert
+    flash :success, t(:'system/configuration/create_admin_penpal/success', :rjobid => rjobid)
+
+    # disable maintenance mode if it wasn't already enabled
+    unless maint_enabled
+      maint_cfg.value = 'no'
+      maint_cfg.save
+    end
+
+    # do an app restart if we need to
+    if ReConnect::ServerUtils.app_server_has_multiple_workers?
+      ReConnect::ServerUtils.app_server_restart!
+    end
+
+    return redirect url("/system/penpal/#{admin_penpal.id}")
   end
 end
