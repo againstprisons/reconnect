55a33731849dc289a598b3926df7cfada333e3c5
Alice Jenkinson <alice@jenkinson.nz>
2019-10-18 15:19:07 +1300

    system/user: Show users with group roles even if they don't have any user roles

--- app/controllers/system_user_by_role_controller.rb
+++ app/controllers/system_user_by_role_controller.rb
@@ -35,43 +35,41 @@ class ReConnect::Controllers::SystemUserByRoleController < ReConnect::Controller
       }
     end
 
+    @groups = ReConnect::Models::Group.all.map do |group|
+      group.user_groups.each do |ug|
+        user_map[ug.user_id] ||= []
+      end
+
+      data = {
+        :group => group,
+        :link => url("/system/group/#{group.id}"),
+        :name => group.decrypt(:name),
+        :roles => group.group_roles,
+      }
+
+      [group.id, data]
+    end.to_h
+
     @users = user_map.map do |uid, user_roles|
       user = ReConnect::Models::User[uid]
       next nil unless user
-
-      groups = ReConnect::Models::UserGroup.where(:user_id => user.id).all.map do |ug|
-        group = ug.group
-        group_roles = group.group_roles.each do |gr|
-          {
-            :gr => gr,
-            :role => gr.role,
-          }
-        end
-
-        {
-          :ug => ug,
-          :group => group,
-          :link => url("/system/group/#{group.id}"),
-          :name => group.decrypt(:name),
-          :roles => group_roles,
-        }
-      end
 
       {
         :user => user,
         :link => url("/system/user/#{user.id}"),
         :name => user.get_name,
         :pseudonym => user.get_pseudonym,
-        :user_roles => user_roles,
-        :groups => groups,
+        :user_roles => user.user_roles,
+        :groups => user.user_groups.map(&:group_id),
       }
-    end
+    end.compact
 
     @title = t(:'system/user/by_role/title')
     return haml(:'system/layout', :locals => {:title => @title}) do
       haml(:'system/user/by_role', :layout => false, :locals => {
         :title => @title,
         :users => @users,
+        :groups => @groups,
       })
     end
   end
