1f70b1561f862212aa47ec6c8213296494c367d3
Lauren Jenkinson <lauren@kat.net.nz>
2020-08-13 16:25:54 +1200

    auth/login: Move TOTP MFA to own controller, merge recovery code handling
    This starts the process of renaming "two-factor" to "multi-factor".
    
    This also integrates the recovery code handling into the same form,
    allowing typing a recovery code into the TOTP field, which brings it
    in line with how EARMMS currently handles this. Currently, any input
    over 8 characters (after removing spaces and hyphens) is assumed to be
    a recovery code.

--- app/controllers/auth_login_controller.rb
+++ app/controllers/auth_login_controller.rb
@@ -1,10 +1,6 @@
 class ReConnect::Controllers::AuthLoginController < ReConnect::Controllers::ApplicationController
   add_route :get, "/"
   add_route :post, "/"
-  add_route :get, "/twofactor", :method => :twofactor
-  add_route :post, "/twofactor", :method => :twofactor
-  add_route :get, "/twofactor/recovery", :method => :twofactor_recovery
-  add_route :post, "/twofactor/recovery", :method => :twofactor_recovery
 
   def index
     return redirect "/" if logged_in?
@@ -52,7 +48,7 @@ class ReConnect::Controllers::AuthLoginController < ReConnect::Controllers::Appl
 
     if user.totp_enabled
       session[:twofactor_uid] = user.id
-      return redirect to("/auth/twofactor")
+      return redirect to("/auth/mfa/totp")
     end
 
     # if we get here, user has successfully logged in
@@ -69,103 +65,5 @@ class ReConnect::Controllers::AuthLoginController < ReConnect::Controllers::Appl
     after_login = session.delete(:after_login)
     return redirect after_login if after_login
     redirect to("/")
-  end
-
-  def twofactor
-    return redirect "/" if logged_in?
-
-    uid = session[:twofactor_uid]
-    return redirect to("/auth") unless uid
-
-    user = ReConnect::Models::User[uid]
-    return redirect to("/auth") unless user
-    return redirect to("/auth") unless user.totp_enabled
-    return redirect to("/auth") if user.totp_secret.nil?
-
-    @title = t(:'auth/login/twofactor/title')
-
-    if request.get?
-      return haml(:'auth/layout', :locals => {:title => @title, :auth_no_tabs => true}) do
-        haml(:'auth/login/twofactor', :layout => false, :locals => {
-          :title => @title,
-        })
-      end
-    end
-
-    rotp = ROTP::TOTP.new(user.decrypt(:totp_secret), :issuer => site_name.gsub(":", "_"))
-    totp_code = request.params["totp_code"]&.strip&.downcase
-    if rotp.verify(totp_code, drift_behind: 15).nil?
-      # invalid totp code
-      flash :error, t(:'auth/login/twofactor/invalid_code')
-      return redirect request.path
-    end
-
-    # if we get here, user has successfully logged in
-    session.delete(:twofactor_uid)
-    token = user.login!
-    session[:token] = token.token
-
-    if user.preferred_language
-      lang = user.decrypt(:preferred_language)
-      session[:lang] = lang
-    end
-
-    flash :success, t(:'auth/login/success', :site_name => site_name)
-
-    after_login = session.delete(:after_login)
-    return redirect after_login if after_login
-    redirect to("/")
-  end
-
-  def twofactor_recovery
-    return redirect "/" if logged_in?
-
-    uid = session[:twofactor_uid]
-    return redirect to("/auth") unless uid
-
-    user = ReConnect::Models::User[uid]
-    return redirect to("/auth") unless user
-    return redirect to("/auth") unless user.totp_enabled
-    return redirect to("/auth") if user.totp_secret.nil?
-
-    @title = t(:'auth/login/twofactor/recovery/title')
-
-    if request.get?
-      return haml(:'auth/layout', :locals => {:title => @title, :auth_no_tabs => true}) do
-        haml(:'auth/login/twofactor_recovery', :layout => false, :locals => {
-          :title => @title,
-        })
-      end
-    end
-
-    # check recovery code
-    code = request.params["code"]&.strip&.downcase
-    if code.nil? || code.empty?
-      flash :error, t(:'auth/login/twofactor/recovery/invalid_code')
-      return redirect request.path
-    end
-
-    code = code.split(" ").join
-    token = ReConnect::Models::Token.where(:user_id => user.id, :token => code, :use => "twofactor_recovery").first
-    if token.nil? || !token.check_validity!()
-      flash :error, t(:'auth/login/twofactor/recovery/invalid_code')
-      return redirect request.path
-    end
-
-    # code is okay
-    token.invalidate!
-
-    # if we get here, user has successfully logged in
-    session.delete(:twofactor_uid)
-    token = user.login!
-    session[:token] = token.token
-
-    if user.preferred_language
-      lang = user.decrypt(:preferred_language)
-      session[:lang] = lang
-    end
-
-    flash :success, t(:'auth/login/twofactor/recovery/success', :site_name => site_name)
-    redirect to("/account/twofactor")
   end
 end
---
+++ app/controllers/auth_login_totp_controller.rb
@@ -0,0 +1,72 @@
+class ReConnect::Controllers::AuthLoginTotpController < ReConnect::Controllers::ApplicationController
+  add_route :get, "/"
+  add_route :post, "/"
+
+  def index
+    return redirect "/" if logged_in?
+
+    uid = session[:twofactor_uid]
+    return redirect to("/auth") unless uid
+
+    user = ReConnect::Models::User[uid]
+    return redirect to("/auth") unless user
+    return redirect to("/auth") unless user.totp_enabled
+    return redirect to("/auth") if user.totp_secret.nil? || user.totp_secret&.empty?
+
+    @title = t(:'auth/login/mfa/title')
+
+    if request.get?
+      return haml(:'auth/layout', :locals => {:title => @title, :auth_no_tabs => true}) do
+        haml(:'auth/login/mfa/totp', :layout => false, :locals => {
+          :title => @title,
+          :has_security_key => false, # TODO: security key support
+        })
+      end
+    end
+
+    code = request.params["code"]&.strip&.downcase
+    code = code.split(" ").map{|x| x.split('-')}.flatten.join
+
+    # check for recovery code
+    is_recovery = false
+    if code.length > 8
+      token = ReConnect::Models::Token.where(:user_id => user.id, :token => code, :use => "twofactor_recovery").first
+      if token.nil? || !(token.check_validity!())
+        # invalid code
+        flash :error, t(:'auth/login/mfa/totp/errors/invalid_code')
+        return redirect request.path
+      end
+
+      is_recovery = true
+    else
+      rotp = ROTP::TOTP.new(user.decrypt(:totp_secret), :issuer => site_name.gsub(":", "_"))
+      if rotp.verify(code, drift_behind: 15).nil?
+        # invalid code
+        flash :error, t(:'auth/login/mfa/totp/errors/invalid_code')
+        return redirect request.path
+      end
+    end
+
+    # if we get here, user has successfully logged in
+    session.delete(:twofactor_uid)
+    token = user.login!
+    session[:token] = token.token
+
+    if user.preferred_language
+      lang = user.decrypt(:preferred_language)
+      session[:lang] = lang
+    end
+
+    # redirect away to MFA settings if recovery code was used
+    if is_recovery
+      flash :success, t(:'auth/login/mfa/recovery/success', :site_name => site_name)
+      return redirect to("/account/twofactor")
+    end
+
+    flash :success, t(:'auth/login/success', :site_name => site_name)
+
+    after_login = session.delete(:after_login)
+    return redirect after_login if after_login
+    redirect to("/")
+  end
+end
