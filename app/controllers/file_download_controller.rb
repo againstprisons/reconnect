f75064be622e8a0a78d37a2fb8259329e8e28fa0
Lauren Jenkinson <lauren@iris.ac.nz>
2020-10-07 19:14:27 +1300

    filedl: Only do user check if a user ID is present on token

--- app/controllers/file_download_controller.rb
+++ app/controllers/file_download_controller.rb
@@ -2,16 +2,19 @@ class ReConnect::Controllers::FileDownloadController < ReConnect::Controllers::A
   add_route :get, "/:fileid/:token"
 
   def index(fid, token)
-    return halt 404 unless logged_in?
-    @user = current_user
-
-    @token = ReConnect::Models::Token.where(:token => token, :use => 'file_download', :user_id => current_user.id).first
+    @token = ReConnect::Models::Token.where(:token => token, :use => 'file_download').first
     return halt 404 unless @token
     return halt 404 unless @token.check_validity!
+
+    if @token.user_id
+      return halt 404 unless @token.user_id == current_user&.id
+    end
 
     @file = ReConnect::Models::File.where(:file_id => fid).first
     return halt 404 unless @file
     return halt 404 unless @token.extra_data == @file.file_id
+
+    @token.invalidate!
 
     data = @file.decrypt_file
 
