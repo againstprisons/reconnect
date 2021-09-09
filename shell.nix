6f6a938f9a064a5fca34004dcd4f9e6fc361e434
Iris System <iris@iris.ac.nz>
2021-09-09 17:20:25 +1200

    nix: pull in shell.nix from againstprisons/kukupa

--- shell.nix
+++ shell.nix
@@ -1,16 +1,29 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
 
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    ruby
-    bundler
-  ];
+with lib;
 
-  buildInputs = with pkgs; [
+let
+  neededLibraries = with pkgs; [
+    curl
     libsodium
     libxml2
     libxslt
     postgresql
-    sqlite
   ];
+
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ruby
+    bundler
+
+    nodejs
+    nodePackages.npm
+
+    pkg-config
+    shared-mime-info
+  ] ++ neededLibraries;
+
+  "LD_LIBRARY_PATH" = makeLibraryPath neededLibraries;
+  "FREEDESKTOP_MIME_TYPES_PATH" = "${pkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml";
 }
