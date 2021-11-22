3308c3aa8acba32a9f45243ac885ed63b1814249
Iris System <iris@iris.ac.nz>
2021-11-22 14:55:21 +1300

    helpers/system_penpal_search: allow excluding test penpals from search

--- app/helpers/system_penpal_search_helpers.rb
+++ app/helpers/system_penpal_search_helpers.rb
@@ -5,6 +5,7 @@ module ReConnect::Helpers::SystemPenpalSearchHelpers
     penpal_ids = []
     search_opts = opts[:search_opts] || {}
     search_opts[:mode] ||= :any
+    search_opts[:ignore_test] = true unless search_opts.key?(:ignore_test)
 
     query_parts = Shellwords.split(query)
     query_parts.each do |part|
@@ -12,8 +13,15 @@ module ReConnect::Helpers::SystemPenpalSearchHelpers
       if part == 'all'
         search_opts[:mode] = :all
         next
+      end 
+
+      # Allow toggling test penpal inclusion
+      if part == '?test'
+        search_opts[:ignore_test] = !search_opts[:ignore_test]
+        next
       end
 
+      # Split search parts
       part_type, part_param = part.split(':', 2)
 
       # Match on search part type
@@ -45,14 +53,28 @@ module ReConnect::Helpers::SystemPenpalSearchHelpers
       end
     end
 
+    # Reduce/flatten penpal ID list
     case search_opts[:mode]
     when :all
       # Return list of IDs that match all
-      penpal_ids.reduce(:&).uniq
+      final_penpal_ids = penpal_ids.reduce(:&).uniq
 
     else
       # Return flattened list
-      penpal_ids.flatten.uniq
+      final_penpal_ids = penpal_ids.flatten.uniq
     end
+
+    # Potentially filter out test penpals
+    if search_opts[:ignore_test]
+      final_penpal_ids.map! do |ppid|
+        pp = ReConnect::Models::Penpal[ppid]
+        next nil if pp.decrypt(:prisoner_number).start_with?('test')
+
+        ppid
+      end
+    end
+
+    # And return the final list
+    final_penpal_ids.compact
   end
 end
