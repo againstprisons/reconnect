31e548e9bcf2ba2362137329b28448905855cfbd
Iris System <iris@iris.ac.nz>
2021-11-29 18:33:04 +1300

    workers/correspondence_card_queue_generate: init app before worker

--- app/workers/correspondence_card_queue_generate_worker.rb
+++ app/workers/correspondence_card_queue_generate_worker.rb
@@ -2,6 +2,9 @@ class ReConnect::Workers::CorrespondenceCardQueueGenerateWorker
   include Sidekiq::Worker
 
   def perform
+    ReConnect.initialize if ReConnect.app.nil?
+    ReConnect.app_config_refresh(:force => true)
+
     query = ReConnect::Models::Correspondence
       .select(:id, :card_status)
       .where(card_status: 'ready')
