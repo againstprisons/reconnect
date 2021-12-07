43187120cfd7f0ee587d605615f306870997f159
Iris System <iris@iris.ac.nz>
2021-12-07 16:57:54 +1300

    holidaycard: Forcibly exclude test PRNs from display

--- app/helpers/holiday_card_helpers.rb
+++ app/helpers/holiday_card_helpers.rb
@@ -6,26 +6,28 @@ module ReConnect::Helpers::HolidayCardHelpers
 
     current_count, counts = 0, []
     while counts.length < opts[:n] && current_count <= opts[:count_max]
-      counts << ReConnect::Models::HolidayCardCount
+      this_count = ReConnect::Models::HolidayCardCount
         .where({ :card_instance => instance, opts[:count_field] => current_count })
         .order(Sequel.lit('RANDOM()'))
         .limit(opts[:n])
         .all
 
+      this_count = this_count.map do |c|
+        next nil if c[opts[:count_field]] >= opts[:count_max]
+        penpal = ReConnect::Models::Penpal[c.penpal_id]
+        next nil if penpal.decrypt(:prisoner_number).start_with?("test")
+
+        {
+          cobj: c,
+          penpal: penpal
+        }
+      end.compact
+
       current_count += 1
+      counts << this_count
       counts.flatten!
     end
 
-    counts.reject! do |c|
-      c[opts[:count_field]] >= opts[:count_max]
-    end
-
-    # Get the penpal objects
-    counts.map do |cobj|
-      {
-        cobj: cobj,
-        penpal: ReConnect::Models::Penpal[cobj.penpal_id],
-      }
-    end.shuffle
+    counts.shuffle
   end
 end
