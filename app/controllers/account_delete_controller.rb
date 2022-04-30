fda491ab136b847559fde4eea587d2cea5bbf6c1
Iris System <iris@iris.ac.nz>
2022-04-30 12:13:38 +1200

    account/delete: Harden flow against "accidental" deletions
    Why is this an issue. Why.

--- app/controllers/account_delete_controller.rb
+++ app/controllers/account_delete_controller.rb
@@ -12,31 +12,50 @@ class ReConnect::Controllers::AccountDeleteController < ReConnect::Controllers::
     @user = current_user
     @title = t(:'account/delete/title')
 
+    # Generate a verification code and store it in the session if one
+    # doesn't already exist there
+    if session.key?(:account_delete_verifycode)
+      @verify_code = session[:account_delete_verifycode]
+    else
+      @verify_code = Random.new.rand(100000000 .. 999999999).to_s
+      session[:account_delete_verifycode] = @verify_code
+    end
+
     if request.get?
       return haml(:'account/layout', :locals => {:title => @title}) do
         haml(:'account/delete', :layout => false, :locals => {
           :title => @title,
           :user => @user,
+          :verify_code => @verify_code,
         })
       end
     end
 
+    # Check verification code
+    form_verify = request.params['verify']&.strip
+    form_verify = form_verify&.split(' ')&.map{|x| x.split('-')}&.flatten&.join('')
+    if form_verify != session.delete(:account_delete_verifycode)
+      flash :error, t(:'account/delete/errors/invalid_code')
+      return redirect request.path
+    end
+
+    # Check password
     password = request.params["password"]&.strip
     if password.nil? || password&.empty?
       flash :error, t(:'account/delete/errors/invalid_password')
       return redirect request.path
-    end
-
-    unless @user.password_correct?(password)
-      flash :error, t(:'account/delete/errors/invalid_password')
-      return redirect request.path
+    else
+      unless @user.password_correct?(password)
+        flash :error, t(:'account/delete/errors/invalid_password')
+        return redirect request.path
+      end
     end
 
     # Mark account for soft deletion
     @user.soft_delete!
 
     # Invalidate and remove session token
-    current_token.invalidate!
+    @user.invalidate_tokens!
     session.delete(:token)
 
     # Redirect to index
