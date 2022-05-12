59d8a9fc5633236b9f4eb4acf69e7473843f60ee
Iris System <iris@iris.ac.nz>
2022-05-12 18:04:45 +1200

    workers/email_queue_send: poke it with a stick until it works

--- app/workers/email_queue_send_worker.rb
+++ app/workers/email_queue_send_worker.rb
@@ -1,19 +1,25 @@
 class ReConnect::Workers::EmailQueueSendWorker
   include Sidekiq::Worker
 
+  # XXX: The way emails work in re:connect right now is horrible and fragile,
+  # and is going to be completely rewritten _very_ soon.
+
   def perform
     ReConnect.initialize if ReConnect.app.nil?
     ReConnect.app_config_refresh(:force => true)
 
-    messages = ReConnect::Models::EmailQueue.where(:queue_status => 'queued')
-    logger.info("Queued message count: #{messages.count}")
+    queue_ids = ReConnect::Models::EmailQueue
+      .select(:id, :queue_status)
+      .where(queue_status: 'queued')
+      .map(&:id)
 
-    messages.all.each do |qm|
-      logger.info("Message #{qm.id}: starting")
+    logger.info("Starting with #{queue_ids.count} in queue")
 
-      # save state as 'sending'
-      qm.queue_status = 'sending'
-      qm.save
+    queue_ids.each do |qmid|
+      qm = ReConnect::Models::EmailQueue[qmid]
+      next unless qm.queue_status = 'queued'
+      qm.update(queue_status: 'sending')
+      logger.info("Message #{qm.id}: starting send")
 
       begin
         chunks = qm.generate_messages_chunked
@@ -24,18 +30,15 @@ class ReConnect::Workers::EmailQueueSendWorker
           chunks[i].deliver!
         end
 
-        qm.queue_status = 'sent'
-        qm.save
-
+        qm.update(queue_status: 'sent')
         logger.info("Message #{qm.id}: sent!")
 
       rescue => e
-        qm.queue_status = 'error'
-        qm.save
+        qm.update(queue_status: 'error')
 
         errmsg = "Message #{qm.id}: errored - #{e.class.name}: #{e}\n"
         errmsg += "Traceback: #{e.traceback}" if e.respond_to?(:traceback)
-        logger.error(errmsg)
+        logger.error(errmsg.strip)
       end
     end
   end
