33a1565220a9ef5ace8c4ec073b3dba38cb0a1ca
Iris System <iris@iris.ac.nz>
2023-08-11 14:31:06 +1200

    helpers/sitealert: add generic alert email helper

--- app/helpers/site_alert_helpers.rb
+++ app/helpers/site_alert_helpers.rb
@@ -4,4 +4,25 @@ module ReConnect::Helpers::SiteAlertHelpers
     return true if ReConnect.app_config['site-alert-emails']['alerts'].include?('*')
     ReConnect.app_config['site-alert-emails']['alerts'].include?(type.to_s)
   end
+
+  def make_generic_alert_email(type, message, extra_data = {})
+    return nil unless should_send_alert_email(type)
+
+    data = {
+      alert_type: type,
+      alert_message: message,
+      alert_data: extra_data,
+    }
+
+    email = ReConnect::Models::EmailQueue.new_from_template("alert_generic", data)
+    email.queue_status = "queued"
+    email.encrypt(:subject, "Site alert: #{type}")
+    email.encrypt(:recipients, JSON.dump({
+      mode: 'list',
+      list: [ReConnect.app_config['site-alert-emails']['email']]
+    }))
+
+    email.save
+    email
+  end
 end
